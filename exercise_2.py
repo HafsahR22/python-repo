10306f8fc32f196b0326f8c883c6ef2323178321
Hafsah Robleh <hafsah.robleh@gmail.com>
2023-06-10 14:41:52 +0000

    exercise_2 final

--- exercise_2.py
+++ exercise_2.py
@@ -2,5 +2,5 @@ income = 250_000
 lowtaxland_rate = income * 0.05 
 lowtaxland_rate = income * 0.05 
 ripoffland_rate = income * 0.43
-st = rt - lt 
-print ('Your income is',income, 'and you would pay',lowland_rate,'income tax in Lowtaxland or',ripoffland_rate, 'income tax in Ripoffland. You would save',,st, 'by paying taxes in Lowtaxland!')
+st = ripoffland_rate - lowtaxland_rate
+print ('Your income is',income,'and you would pay',lowtaxland_rate,'income tax in Lowtaxland or',ripoffland_rate,'income tax in Ripoffland. You would save',st,'by paying taxes in Lowtaxland!')
